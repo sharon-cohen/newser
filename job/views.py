068ec264e747168515d8798ce5f2e0081b9496d1
s.cohen <s.cohen@aligntech.com>
2021-05-13 17:52:06 +0300

    dfdfd

--- job/views.py
+++ job/views.py
@@ -5,15 +5,20 @@ from pywand import Wand, wm_msg, WandPowerStates, WandStates
 
 def index(request):
     jobs = Job.objects.all()
-    wand = Wand("192.168.1.100")
-    while True:
-        time.sleep(2)
-        print(wand.get_state()[0])
-        if wand.get_state()[0] == WandStates.WS_STANDBY:
-
-            break
+    
+   
     return render(request, 'job/home.html', {'jobs': jobs})
 def start(request):
     jobs = Job.objects.all()
+    try:
+        wand = Wand("192.168.1.100")
+        while True:
+            time.sleep(2)
+            print(wand.get_state()[0])
+            if wand.get_state()[0] == WandStates.WS_STANDBY:
+
+                break
+    except:
+        print("fail")
     print("start")
     return render(request, 'job/home.html', {'jobs': jobs})
