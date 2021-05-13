efbc1a72d313e50795ad4e3e06f8e23deb970ccd
s.cohen <s.cohen@aligntech.com>
2021-05-13 17:47:05 +0300

    wand

--- job/views.py
+++ job/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Job
-
+import time
+from pywand import Wand, wm_msg, WandPowerStates, WandStates
 
 def index(request):
     jobs = Job.objects.all()
-    
+    wand = Wand("192.168.1.100")
+    while True:
+        time.sleep(2)
+        print(wand.get_state()[0])
+        if wand.get_state()[0] == WandStates.WS_STANDBY:
+
+            break
     return render(request, 'job/home.html', {'jobs': jobs})
 def start(request):
     jobs = Job.objects.all()
